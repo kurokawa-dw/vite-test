a91a60ca60fb7823ba7aa57c7592c5436d284f14
Kurokawa <keiichi@kurokawa-dw.com>
2023-07-18 00:51:58 +0900

    publicディレクトリでjsの挙動を試す、ホットリロードきかない、nodemoduleのライブラリ使えない

---
+++ public/assets/javascripts/index/moule.js
@@ -0,0 +1,5 @@
+const test = () => {
+	console.log('テストexport');
+}
+
+export default test;
--- public/assets/javascripts/index/script.js
+++ public/assets/javascripts/index/script.js
@@ -1,1 +1,9 @@
-console.log('パブリックですよ')
+console.log('パブリックですよffff');
+console.log('hotリロード効かないからね')
+console.log('nodemoduleからインポートできないからね')
+
+import test from './moule.js';
+
+test();
+
+console.log('これはインポートできる、けどホットリロードは動かないよ')
